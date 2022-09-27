2a2f631d263e460dc7af2888ff7f605b171438fa
david_dd <demuriya.ds@phystech.ru>
2022-09-28 02:46:39 +0300

    compilable

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include <functional>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
 
-int main(){
-    std::cout << "Hello world!" << std::endl;
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode({200, 200}), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
     return 0;
 }
